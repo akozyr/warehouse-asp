e202301beec0b80f7f87a1c5a9958765e6ef9bd4
akozyr <anton.kozyr.dev@gmail.com>
2017-03-13 22:31:22 +0200

    Initial commit (base structure is created)

---
+++ warehouse-asp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="warehouse_asp.MvcApplication" Language="C#" %>
---
+++ warehouse-asp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(warehouse_asp.Startup))]
+namespace warehouse_asp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
