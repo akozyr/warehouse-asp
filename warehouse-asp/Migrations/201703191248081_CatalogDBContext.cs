715cd7499e5060f5bb29cbc010b1d4a43d0bab44
Sergey <gerra.lampdev@gmail.com>
2017-03-19 14:53:01 +0200

    * Add `Catalog` and `Product` models

---
+++ warehouse-asp/Migrations/201703191248081_CatalogDBContext.cs
@@ -0,0 +1,41 @@
+namespace warehouse_asp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CatalogDBContext : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Catalogs",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                        price = c.Single(nullable: false),
+                        catalogId = c.Int(),
+                    })
+                .PrimaryKey(t => t.id)
+                .ForeignKey("dbo.Catalogs", t => t.catalogId)
+                .Index(t => t.catalogId);            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "catalogId", "dbo.Catalogs");
+            DropIndex("dbo.Products", new[] { "catalogId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Catalogs");
+        }
+    }
+}
---
+++ warehouse-asp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace warehouse_asp.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float price { get; set; }
+
+        // foreing key
+        public int? catalogId { get; set; }
+        // navigational property
+        public Catalog Catalog { get; set; }
+
+        public class ProductDBContext : ApplicationDbContext
+        {
+            public DbSet<Product> Products { get; set; }
+        }
+    }
+}
