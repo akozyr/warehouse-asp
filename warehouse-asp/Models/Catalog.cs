f76d930588515f9d58dd889f4df32fc5e395d38d
akozyr <anton.kozyr.dev@gmail.com>
2017-03-19 13:00:26 +0200

    * Remove some template functional * Add `Catalog` and `Product` models * Add migrations

---
+++ warehouse-asp/Models/Catalog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace warehouse_asp.Models
+{
+    public class Catalog
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+        public Catalog()
+        {
+            Products = new List<Product>();
+        }
+    }
+
+    public class CatalogDBContext : DbContext
+    {
+        public DbSet<Catalog> Catalogs { get; set; }
+    }
+}
